d0c1bdaa180031dbd61c354ed9e5a312b44b2806
Steeeeve <57923724+steven-hosking@users.noreply.github.com>
2021-09-11 16:59:54 +1000

    Add files via upload

---
+++ packageinfo.py
@@ -0,0 +1,20 @@
+# Name of the package.
+NAME         = 'PersonalDetails'
+# Version of the package.
+VERSION      = '1.0.0'
+# Description about the package.
+DESCRIPTION  = 'Technical Task.'
+# Keywords to find this package.
+KEYWORDS     = []
+# Platform.
+PLATFORMS    = ['Windows']
+# Applications which this package meant to be initialized for.
+APPLICATIONS = ['all']
+# Is the application in use.
+IS_ACTIVE    = True
+# Third party application.
+IS_EXTERNAL  = False
+# Developer.
+DEVELOPERS   = ['Steven Hosking']
+# Dependencies.
+DEPENDENCIES = []
